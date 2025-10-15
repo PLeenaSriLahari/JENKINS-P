3768b5beae7a71c37914ea06fcdd6352cff14030
LSL <2300031012@kluniversity.in>
2025-10-15 16:58:36 +0530

    Updated backend

--- Festivalapi-SpringBoot/src/main/java/com/example/practice/FestivalapiSpringBootApplication.java
+++ Festivalapi-SpringBoot/src/main/java/com/example/practice/FestivalapiSpringBootApplication.java
@@ -2,6 +2,7 @@ package com.example.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class FestivalapiSpringBootApplication extends SpringBootServletInitializer {
