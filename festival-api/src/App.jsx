2bcec719ef752d4ae69a3fe87f6de26985adc729
LSL <2300031012@kluniversity.in>
2025-10-17 14:33:08 +0530

    Updated

--- festival-api/src/App.jsx
+++ festival-api/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import FestivalManager from './components/FestivalManager';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
